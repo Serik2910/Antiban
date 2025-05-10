ed4ddc48c3f12ae023d84a98551612b009962517
kobla <kolandy@inbox.ru>
2025-05-10 22:04:35 +0500

    1

--- Antiban/Antiban.cs
+++ Antiban/Antiban.cs
@@ -1,15 +1,76 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Antiban
 {
     public class Antiban
     {
+
+        private List<AntibanResult> result = new List<AntibanResult>();
+        private List<EventMessage> eventMessages = new List<EventMessage>();
         /// <summary>
         /// Добавление сообщений в систему, для обработки порядка сообщений
         /// </summary>
         /// <param name="eventMessage"></param>
         public void PushEventMessage(EventMessage eventMessage)
         {
+            if (eventMessage != null)
+            {
+                if(result.Count > 0) {
+                    //TODO
+                    //Example
+                    //var lastEventMessage = result[result.Count - 1];
+                    DateTime targetDateTime = eventMessage.DateTime;
+                    var res = from r in result
+                              join e in eventMessages on r.EventMessageId equals e.Id
+                              select new { SentDateTime = r.SentDateTime, EventMessageId = r.EventMessageId, DateTime = e.DateTime, Phone = e.Phone, Priority = e.Priority};
+                    var result_ = res.ToList();
+
+                    if (eventMessage.Priority == 1)
+                    {
+                        var eventMessageWithSameNumberWithinDay = result_.FindLast(x =>
+                            (x.Phone == eventMessage.Phone) && (x.Priority == 1) &&
+                            Math.Abs((x.DateTime - targetDateTime).TotalHours) < 24);
+                        while (eventMessageWithSameNumberWithinDay != null)
+                        {
+                            targetDateTime = eventMessageWithSameNumberWithinDay.DateTime.AddHours(24);
+                            eventMessageWithSameNumberWithinDay = result_.Find(x =>
+                                (x.Phone == eventMessage.Phone) && (x.Priority == 1) && Math.Abs((x.DateTime - targetDateTime).TotalHours) < 24);
+                        }
+                    }
+                    var eventMessageWithSameNumberWithinMinute = result_.FindLast(x =>
+                        (x.Phone == eventMessage.Phone) &&
+                        Math.Abs((x.DateTime - targetDateTime).TotalSeconds) < 60);
+
+                    while (eventMessageWithSameNumberWithinMinute != null)
+                    {
+                        targetDateTime = eventMessageWithSameNumberWithinMinute.DateTime.AddMinutes(1);
+                        eventMessageWithSameNumberWithinMinute = result_.Find(x =>
+                            (x.Phone == eventMessage.Phone) &&
+                            Math.Abs((x.DateTime - targetDateTime).TotalSeconds) < 60);
+                    };
+
+                    
+                    var lastEventMessageWithDifferentNumberWithinTenSeconds = result_.Find(x =>
+                        (x.Phone != eventMessage.Phone) && Math.Abs((x.DateTime - targetDateTime).TotalSeconds) < 10);
+                    while (lastEventMessageWithDifferentNumberWithinTenSeconds != null)
+                    {
+                        targetDateTime = lastEventMessageWithDifferentNumberWithinTenSeconds.DateTime.AddSeconds(10);
+                        lastEventMessageWithDifferentNumberWithinTenSeconds = result_.Find(x =>
+                            (x.Phone != eventMessage.Phone) && Math.Abs((x.DateTime - targetDateTime).TotalSeconds) < 10);
+                    }
+
+                    eventMessage.DateTime = targetDateTime;
+                }
+                eventMessages.Add(eventMessage);
+                result.Add(new AntibanResult()
+                {
+                    SentDateTime = eventMessage.DateTime,
+                    EventMessageId = eventMessage.Id
+                });
+                result.Sort((x, y) => x.SentDateTime.CompareTo(y.SentDateTime));
+            }
             //TODO
         }
 
@@ -19,14 +80,6 @@ namespace Antiban
         /// <returns></returns>
         public List<AntibanResult> GetResult()
         {
-            //TODO
-            //Example
-            var result = new List<AntibanResult>();
-            for (int i = 0; i < 10; i++)
-            {
-                result.Add(new AntibanResult());
-            }
-            
             return result;
         }
     }
